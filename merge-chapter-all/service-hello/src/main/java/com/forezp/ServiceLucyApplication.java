9754355cf4f4ef49fba13df61a78d2f1449df932
hill <shu297>
2017-10-06 10:18:12 +0800

    add merge chapters all project

---
+++ merge-chapter-all/service-hello/src/main/java/com/forezp/ServiceLucyApplication.java
@@ -0,0 +1,36 @@
+package com.forezp;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
+public class ServiceLucyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceLucyApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+	@RequestMapping("/hello")
+	@HystrixCommand(fallbackMethod = "helloError")
+	public String home(@RequestParam String name) {
+		return "hello "+name+",i  am hello and from port:" +port;
+	}
+
+	public String helloError(String name) {
+		return "hello,"+name+",sorry,error!";
+	}
+}
